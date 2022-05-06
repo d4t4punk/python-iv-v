00abb289b57299e9c4c7a728ccdca3105bc272ba
D4t4Mon <scott.newby74@gmail.com>
2022-05-06 17:06:28 -0400

    initial code

---
+++ python-scripts/main.py
@@ -0,0 +1,71 @@
+# Scott Newby - 2022-05-06
+# Description
+# Python & PostreSQL Independent Validation & Verification (IVV)
+# Main.py provides an entry point to kick off source & target
+# processes and to produce the IVV Excel artifacts
+# this project is meant to be a framework for an operator to fill-in with 
+# SQL scripts and to be customizable to various sources and targets
+# There is a companion postgreSQL to postgreSQL small databawse
+# migration project embedded to exercise and become familiar with the framework 
+# This project was designed and run under miniconda on Linux
+
+
+# Imports
+
+import psycopg2
+import openpyxl
+from openpyxl.styles import Font
+from openpyxl import Workbook
+import getopt
+import sys
+import os
+import datetime
+import yaml
+# import other python scripts
+
+import pgconnect
+
+
+
+
+def main(argv):
+    print('main')
+    # open the config
+    with open('config.yml') as cfg:
+        config = yaml.safe_load(cfg)
+
+    # Get validation configurations out of the config.yml
+    
+    ivvdb = config['IVVDB']['db']
+    ivvpwd = config['IVVDB']['pwd']
+    ivvusr = config['IVVDB']['usr']
+    ivvport = config['IVVDB']['port']
+    ivvhost = config['IVVDB']['host']
+    global pgconnection
+
+    print(ivvdb,ivvpwd,ivvusr,ivvport,ivvhost)
+    
+    pgconnection = pgconnect.pgconnect(ivvdb,ivvusr,ivvpwd,ivvhost,ivvport)
+
+def export_to_excel(qry, headings, filepath):
+    print('export to excel')
+    local_cursor = pgconnection.cursor()
+    local_cursor.execute(qry)
+    exp_data = local_cursor.fetchall()
+    local_cursor.close()
+
+    wb = Workbook()
+    sheet = wb.active
+
+    sheet.row_dimensions[1].font = Font(bold = True)
+
+    for col, heading in enumerate(headings, start = 1):
+        sheet.cell(row = 1, column = col).value = heading
+
+    for row, rows in enumerate(exp_data, start = 2):
+        for col, cell_val in enumerate(rows, start = 1):
+            sheet.cell(rows = row, column = col).value = cell_val
+
+    wb.save(filepath)
+
+main(sys.argv[1:])
